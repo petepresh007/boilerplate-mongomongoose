cad9990dd4df32ddbb405de6d8520b6e81480778
Peter Dozie <peterprecious370@gmail.com>
2023-08-25 19:28:35 -0700

    it is connected

--- myApp.js
+++ myApp.js
@@ -1,7 +1,18 @@
 require('dotenv').config();
 const mongoose = require("mongoose");
+mongoose.set('strictQuery', false);
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("connected to database successfully..")
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
+connectDB()
 
 let Person;
 
