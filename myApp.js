acd169a688543e4fd3b62526153a28815a4bb15d
Peter Dozie <peterprecious370@gmail.com>
2023-08-29 20:50:54 -0700

    yy

--- myApp.js
+++ myApp.js
@@ -30,7 +30,7 @@ let Person = mongoose.model("Person", personSchema);
 
 /**CREATE AND SAVE PEESON */
 const createAndSavePerson = async (done) => {
-  //done(null /*, data*/);
+  done(null /*, data*/);
   // const newPerson = new Person({ name: "John", age: 12, favoriteFoods: ["Onion", "Garlic"] });
   // newPerson.save((err, data) => {
   //   if (err) {
@@ -38,50 +38,28 @@ const createAndSavePerson = async (done) => {
   //   }
   //   done(null, data);
   // });
+};
 
-  //done(null /*, data*/);
-  // const newPerson = new Person({ name: "Man", age: 11, favoriteFoods: ["Coke", "Bread"] });
-  // newPerson.save((err, data) => {
-  //   if (err) {
-  //     done(err);
-  //   }
-  //   done(null, data);
-  // });
+/**CREATE MANY PEOPLE */
+// var arrayOfPeople = [
+//   { name: "Jane", age: 22, favoriteFoods: ["Yam", "Tomato"] },
+//   { name: "Mike", age: 54, favoriteFoods: ["Sogium", "Sweetnwe"] },
+//   { name: "James", age: 47, favoriteFoods: ["Biscuits", "Tea"] },
+// ]
 
-  var arrayOfPeople = [
+const createManyPeople = (arrayOfPeople, done) => {
+  arrayOfPeople = [
     { name: "Jane", age: 22, favoriteFoods: ["Yam", "Tomato"] },
     { name: "Mike", age: 54, favoriteFoods: ["Sogium", "Sweetnwe"] },
     { name: "James", age: 47, favoriteFoods: ["Biscuits", "Tea"] },
   ]
-  Person.create(arrayOfPeople, (err, data)=>{
-    if(err){
+  //done(null /*, data*/);
+  Person.create(arrayOfPeople, function (err, data) {
+    if (err) {
       console.error(err);
     }
-    return done(null, data);
-  })
-
-};
-
-/**CREATE MANY PEOPLE */
-var arrayOfPeople = [
-  { name: "Jane", age: 22, favoriteFoods: ["Yam", "Tomato"] },
-  { name: "Mike", age: 54, favoriteFoods: ["Sogium", "Sweetnwe"] },
-  { name: "James", age: 47, favoriteFoods: ["Biscuits", "Tea"] },
-]
-
-const createManyPeople = (arrayOfPeople, done) => {
-  // arrayOfPeople = [
-  //   { name: "Jane", age: 22, favoriteFoods: ["Yam", "Tomato"] },
-  //   { name: "Mike", age: 54, favoriteFoods: ["Sogium", "Sweetnwe"] },
-  //   { name: "James", age: 47, favoriteFoods: ["Biscuits", "Tea"] },
-  // ]
-  // //done(null /*, data*/);
-  // Person.create(arrayOfPeople, function (err, data) {
-  //   if (err) {
-  //     console.error(err);
-  //   }
-  //   done(null, data);
-  // });
+    done(null, data);
+  });
 };
 
 
